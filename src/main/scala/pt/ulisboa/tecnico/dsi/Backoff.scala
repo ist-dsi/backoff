91a713a072aff562af27f628a59d49c1693fcb4d
Rui Botelho <rui.botelho@tecnico.ulisboa.pt>
2015-11-13 19:43:14 +0000

    finish tests

--- src/main/scala/pt/ulisboa/tecnico/dsi/Backoff.scala
+++ src/main/scala/pt/ulisboa/tecnico/dsi/Backoff.scala
@@ -11,36 +11,64 @@ import com.typesafe.config.{Config, ConfigFactory, ConfigList}
   */
 object Backoff {
   val root = ConfigFactory.load().getConfig("backoff")
-  val goldenRation = (1 - math.sqrt(5)) / 2
+  val goldenRatio = (1 - math.sqrt(5)) / 2
   val iterationExpected = "expected iteration greater or equal than 0"
 
   def getDurationConstant(name: String): FiniteDuration = {
     new FiniteDuration(root.getDuration(name, TimeUnit.MILLISECONDS), TimeUnit.MILLISECONDS)
   }
 
-  def constantFunction(duration: FiniteDuration = getDurationConstant("constant-duration"))
-                      (iteration: Int): FiniteDuration = {
+  /**
+    * Return a backoff duration equal to @param duration for all iterations
+    * @param iteration
+    * @param duration
+    * @return
+    */
+  def constantFunction(iteration: Int)
+                      (duration: FiniteDuration = getDurationConstant("constant-duration"))
+  : FiniteDuration = {
     require(iteration >= 0, iterationExpected)
     duration
   }
 
-  def linearFunction(duration: FiniteDuration =getDurationConstant("linear-constant"))
-                    (iteration: Int): FiniteDuration = {
+  /**
+    * returns a backoff linear duration on the iteration
+    * @param iteration
+    * @param duration
+    * @return
+    */
+  def linearFunction(iteration: Int)
+                    (duration: FiniteDuration = getDurationConstant("linear-constant"))
+  : FiniteDuration = {
     require(iteration >= 0, iterationExpected)
     duration * iteration
   }
 
-  def exponentialFunction(duration: FiniteDuration = getDurationConstant("exponential-constant"))
-                         (iteration: Int): FiniteDuration = {
+  /**
+    * Returns a backoff duration that increases linearly for each iteration
+    * @param iteration
+    * @param duration
+    * @return
+    */
+  def exponentialFunction(iteration: Int)
+                         (duration: FiniteDuration = getDurationConstant("exponential-constant"))
+  : FiniteDuration = {
     require(iteration >= 0, iterationExpected)
     val d = duration * math.exp(iteration)
     d.asInstanceOf[FiniteDuration]
   }
 
-  def fibonacciFunction(duration: FiniteDuration = getDurationConstant("fibonacci-duration"))
-                       (iteration: Int): FiniteDuration = {
+  /**
+    * Return a backoff duration aproximately equal to the fibonacci sequence for each iteration
+    * @param iteration
+    * @param duration
+    * @return
+    */
+  def fibonacciFunction(iteration: Int)
+                       (duration: FiniteDuration = getDurationConstant("fibonacci-duration"))
+  : FiniteDuration = {
     require(iteration >= 0, iterationExpected)
-    val d = duration * math.pow(goldenRation, iteration)
+    val d = duration * math.pow(goldenRatio, iteration)
     d.asInstanceOf[FiniteDuration]
   }
 
--- src/test/scala/pt/ulisboa/tecnico/dsi/BackoffSpec.scala
+++ src/test/scala/pt/ulisboa/tecnico/dsi/BackoffSpec.scala
@@ -10,8 +10,24 @@ with OptionValues with Inside with Inspectors
   * Created by ikea on 11/12/15.
   */
 class BackoffSpec extends FlatSpec with Matchers{
-  "A Backoff" should "using constant function should always return same value" in {
-    val value1 = Backoff.constantFunction()(1)
-    Backoff.constantFunction()(1) should be (value1)
+  "A Backoff" should "always return the same value using the constant function" in {
+    val value1 = Backoff.constantFunction(0)()
+    Backoff.constantFunction(1)() should be (value1)
+  }
+  it should "return increments using a linear function" in {
+    Backoff.linearFunction(0)(5.seconds) should be (0.seconds)
+    Backoff.linearFunction(1)(5.seconds) should be (5.seconds)
+    Backoff.linearFunction(10)(5.seconds) should be (50.seconds)
+   }
+  it should "return exponential increments using a exponential function" in {
+    Backoff.exponentialFunction(0)(1.seconds) should be (1.seconds)
+    Backoff.exponentialFunction(1)(1.seconds) should be (math.exp(1).seconds)
+    Backoff.exponentialFunction(10)(1.seconds) should be (math.exp(10).seconds)
+  }
+  it should "should follow the fibonacci sequence for each iteration" in {
+    val goldenRatio = (1 - math.sqrt(5)) / 2
+    Backoff.fibonacciFunction(0)(1.seconds) should be (math.pow(goldenRatio,0).seconds)
+    Backoff.fibonacciFunction(1)(1.seconds) should be (math.pow(goldenRatio,1).seconds)
+    Backoff.fibonacciFunction(5)(1.seconds) should be (math.pow(goldenRatio,5).seconds)
   }
 }
