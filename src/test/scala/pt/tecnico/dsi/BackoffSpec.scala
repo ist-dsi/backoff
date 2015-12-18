c8c6c6784410450360eb6b443ae1dfd536cab43a
Rui Botelho <rui.botelho@tecnico.ulisboa.pt>
2015-12-18 18:55:55 +0000

    change exponential test to use a fixed integer sequence

--- src/test/scala/pt/tecnico/dsi/BackoffSpec.scala
+++ src/test/scala/pt/tecnico/dsi/BackoffSpec.scala
@@ -1,14 +1,17 @@
 package pt.tecnico.dsi
 
+import org.scalacheck.Gen
 import org.scalatest._
-import org.scalatest.prop.GeneratorDrivenPropertyChecks
+import org.scalatest.prop.{TableDrivenPropertyChecks, GeneratorDrivenPropertyChecks}
 
 import scala.concurrent.duration._
 
 abstract class UnitSpec extends FlatSpec with Matchers
 with OptionValues with Inside with Inspectors
 
-class BackoffSpec extends PropSpec with GeneratorDrivenPropertyChecks with ShouldMatchers {
+class BackoffSpec extends PropSpec with
+  GeneratorDrivenPropertyChecks with
+  ShouldMatchers {
 
   property("Using the constant function should always return the same value") {
     forAll { iteration: Int =>
@@ -28,14 +31,15 @@ class BackoffSpec extends PropSpec with GeneratorDrivenPropertyChecks with Shoul
     }
   }
 
+  val duration = 1.seconds
+  val maxIteration = math.floor(math.log(Long.MaxValue / duration.toNanos) / math.log(2)).toInt
+  val generator = for(n <- Gen.choose(0,maxIteration-1)) yield n
   property("Using the exponential function should return a constant ratio between two iterations") {
-    forAll { iteration: Int =>
-      val duration = 1.seconds
-      val maxIteration = math.floor(math.log(Long.MaxValue / duration.toNanos) / math.log(2)).toInt
-
-      whenever(iteration >= 0 && iteration < maxIteration) {
-        Backoff.exponential(iteration, duration) * 2 shouldBe Backoff.exponential(iteration + 1, duration)
-      }
+    forAll(generator) { iteration =>
+      val x = Backoff.exponential(iteration, duration) * 2
+      val y = Backoff.exponential(iteration + 1, duration)
+      println(s"it:$iteration x:$x y:$y max:$maxIteration")
+      x shouldBe y
     }
   }
 
